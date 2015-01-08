b7ae719977694aad8eb823cb23a596448167be3a
Vince <tschew@gmail.com>
2015-01-08 19:33:42 +0800

    The new Dashboard and Frontpage

--- public/vendor/certtablescript.js
+++ public/vendor/certtablescript.js
@@ -1,12 +1,12 @@
-        $('#certsettingtable').dataTable( {
-            "scrollY": 200,
-            "ScrollX": true,
-            "iDisplayLength": 10,
-            "order": [[ 0, "desc" ]],
-            "scrollCollapse": true,
-            "jQueryUI": true,
-            "aoColumns": [        
-                null, null,
-                { "bSortable": false }
-              ]
-        } );
+$('#certsettingtable').dataTable({
+    "scrollY": 200,
+    "ScrollX": true,
+    "iDisplayLength": 10,
+    "order": [[ 0, "desc" ]],
+    "scrollCollapse": true,
+    "jQueryUI": true,
+    "aoColumns": [        
+        null, null, null
+        { "bSortable": false }
+      ]
+} );
